78bbd5573d9c0b75cd40cf5635bd16e938f92509
ZX夏夜之风 <snwcreations@qq.com>
2022-05-15 05:49:20 +0000

    First implementation ok!

---
+++ src/main/java/snw/rfmtimenotice/Main.java
@@ -0,0 +1,35 @@
+package snw.rfmtimenotice;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import snw.rfm.RunForMoney;
+import snw.rfm.api.events.GamePostStartEvent;
+import snw.rfm.api.events.GameStopEvent;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onGameStart(GamePostStartEvent event) {
+        Bukkit.getScheduler().runTaskTimer(this, () -> Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "游戏时间剩余 " + RunForMoney.getInstance().getGameController().getGameRemainingTime() / 60), 180 * 20L, 180 * 20L);
+    }
+
+    @EventHandler
+    public void onGameStop(GameStopEvent event) {
+        Bukkit.getScheduler().cancelTasks(this);
+    }
+}
